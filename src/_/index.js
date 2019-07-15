2818e26d69911f7a82af41279bacc5ccff05ad5d
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-07-15 16:19:16 +0530

    .show is a better name for this than .toggle

--- src/_/index.js
+++ src/_/index.js
@@ -60,7 +60,7 @@ const $code = $('#code');
 $('.snippet').each(function() {
   const html = prettify($(this).html());
   $(`
-    <div class="toggle">
+    <div class="show">
       <svg width="16" height="16" fill="${
         window.config.isBackgroundLight ? 'var(--dark)' : 'var(--light)'
       }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
