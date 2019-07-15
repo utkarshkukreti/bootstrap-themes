35641e27bbf197ce9a255bef06b71ead76393293
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-07-15 07:05:47 +0530

    add a banner to generated css files using postcss-banner

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
+const version = require('./package.json').version;
+
 module.exports = {
-  plugins: [require('cssnano')()],
+  plugins: [
+    require('cssnano')(),
+    require('postcss-banner')({
+      banner: `\
+Bootstrap-Themes.com v${version} (https://bootstrap-themes.com/)
+Copyright 2019 Utkarsh Kukreti
+Licensed under MIT (https://github.com/utkarshkukreti/bootstrap-themes/blob/master/LICENSE)`,
+      important: true,
+    }),
+  ],
 };
